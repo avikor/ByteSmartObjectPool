29a655201392fee60e3311a8dcb833abcb7d943f
Avi Korzac <avikor13@gmail.com>
2022-12-30 12:07:05 +0200

    Add project files.

---
+++ ByteSmartObjectPool/ByteSmartObjectPool.h
@@ -0,0 +1,95 @@
+﻿#ifndef BYTE_SMART_OBJECT_POOL
+#define BYTE_SMART_OBJECT_POOL
+
+
+#include <array>
+#include <memory>
+#include <functional>
+
+
+namespace BSPool
+{
+    template <typename T>
+    using PoolItem = std::unique_ptr<T, std::function<void(T*)>>;
+
+
+    class max_capacity_exception : public std::bad_alloc
+    {
+    public:
+        char const* what() const override
+        {
+            return "object pool reached max capacity.";
+        }
+    };
+
+
+    template <typename T, std::size_t CAPACITY>
+    class ByteSmartObjectPool
+    {
+    public:
+        ByteSmartObjectPool() noexcept;
+
+        template <typename... Args>
+        [[nodiscard]] PoolItem<T> request(Args&&... args) noexcept(false);
+
+        ~ByteSmartObjectPool() noexcept;
+
+    private:
+        static constexpr std::size_t s_infinity{ std::numeric_limits<std::size_t>::max() }; // signifies a wrap around
+
+        std::array<std::byte, sizeof(T) * CAPACITY> m_pool;
+        std::array<std::size_t, CAPACITY> m_stack;
+        std::size_t m_stackTop;
+        std::size_t m_maxObjsUsed;
+    };
+
+
+    template <typename T, std::size_t CAPACITY>
+    ByteSmartObjectPool<T, CAPACITY>::ByteSmartObjectPool() noexcept
+        : m_pool{}
+        , m_stack{}
+        , m_stackTop{ 0U }
+        , m_maxObjsUsed{ 0U }
+    {
+        for (std::size_t i = 0; i < CAPACITY; ++i)
+        {
+            m_stack[i] = i;
+        }
+    }
+
+    template <typename T, std::size_t CAPACITY>
+    template <typename... Args>
+    PoolItem<T> ByteSmartObjectPool<T, CAPACITY>::request(Args&&... args) noexcept(false)
+    {
+        if (m_stackTop == CAPACITY)
+        {
+            throw max_capacity_exception{};
+        }
+
+        m_maxObjsUsed = std::max(m_maxObjsUsed, m_stackTop);
+
+        ++m_stackTop;
+
+        return { new (&m_pool[m_stack[m_stackTop - 1] * sizeof(T)]) T{ std::forward<Args>(args)... }, [this](T* obj)
+            {
+                std::size_t idx = obj - reinterpret_cast<T*>(m_pool.data());
+
+                --m_stackTop;
+                m_stack[m_stackTop] = idx;
+            }
+        };
+    }
+
+    template <typename T, std::size_t CAPACITY>
+    ByteSmartObjectPool<T, CAPACITY>::~ByteSmartObjectPool() noexcept
+    {
+        for (; m_maxObjsUsed != s_infinity && m_maxObjsUsed >= 0U; --m_maxObjsUsed)
+        {
+            T& obj = reinterpret_cast<T&>(m_pool[m_maxObjsUsed * sizeof(T)]);
+            obj.~T();
+        }
+    }
+}
+
+
+#endif // !BYTE_SMART_OBJECT_POOL
---
+++ ByteSmartObjectPool/ObjectPoolTests.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "ByteSmartObjectPool.h"
+
+
+struct TrivialSturct
+{
+	int i;
+	float f;
+	double d;
+};
+
+
+TEST_CASE("simple int pool", "[ByteSmartObjectPool]")
+{
+	BSPool::ByteSmartObjectPool<int, 2U> simpleIntPool{};
+
+	BSPool::PoolItem<int> pInt1 = simpleIntPool.request(1);
+	REQUIRE(*pInt1 == 1);
+
+	{
+		auto pInt2 = simpleIntPool.request();
+		REQUIRE(*pInt2 == 0);
+
+		REQUIRE_THROWS_AS(simpleIntPool.request(), BSPool::max_capacity_exception);
+	}
+	
+	auto pInt3 = simpleIntPool.request();
+	REQUIRE(*pInt3 == 0);
+}
+
+TEST_CASE("trivial struct pool", "[ByteSmartObjectPool]")
+{
+	BSPool::ByteSmartObjectPool<TrivialSturct, 2U> trivialSturctPool{};
+
+	BSPool::PoolItem<TrivialSturct> trivial1 = trivialSturctPool.request(1, 2.5f, 3.3);
+	REQUIRE(trivial1->i == 1);
+	REQUIRE(trivial1->f == 2.5f);
+	REQUIRE(trivial1->d == 3.3);
+
+	{
+		auto trivial2 = trivialSturctPool.request();
+		REQUIRE(trivial2->i == 0);
+		REQUIRE(trivial2->f == 0.0f);
+		REQUIRE(trivial2->d == 0.0);
+
+		REQUIRE_THROWS_AS(trivialSturctPool.request(), BSPool::max_capacity_exception);
+	}
+
+	auto trivial3 = trivialSturctPool.request();
+	REQUIRE(trivial3->i == 0);
+	REQUIRE(trivial3->f == 0.0f);
+	REQUIRE(trivial3->d == 0.0);
+}
